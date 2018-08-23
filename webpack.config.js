ff1d1741f0ee8e5694d1536682439e12b2c4f51a
guilhermercoz <guilhermercoz@hotmail.com>
2018-08-22 22:05:25 -0300

    Aula 8

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,24 @@ const webpack = require('webpack')
 module.exports = {
   entry: './ex/index.jsx',
   output: {
-      path: __dirname + '/public',
-      filename: '/bundle.js'
+    path: __dirname + '/public',
+    filename: './bundle.js'
   },
   devServer: {
     port: 8080,
     contentBase: './public',
   },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   module: {
     loaders: [{
       test: /.jsx?$/,
       loader: 'babel-loader',
-      exluce: /node_modules/,
+      exclude: /node_modules/,
       query: {
         presets: ['es2015', 'react']
       }
     }]
   }
-
 }
